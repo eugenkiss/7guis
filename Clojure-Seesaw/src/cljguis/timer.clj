32495129dae2108d005140e2932787519cac7717
Eugen Kiss <eugen@eugenkiss.com>
2014-03-21 11:08:52 +0100

    Add timer example for Java-Swing

--- Clojure-Seesaw/src/cljguis/timer.clj
+++ Clojure-Seesaw/src/cljguis/timer.clj
@@ -8,32 +8,32 @@
 (defn format-elapsed [elapsed]
   (str (-> (/ elapsed 10.0) Math/floor int) "." (mod elapsed 10) "s"))
 
-(defn processing [elapsed max]
+(defn processing [elapsed duration]
   (while true
-    (when (< @elapsed @max)
+    (when (< @elapsed @duration)
 	    (Thread/sleep 100)
 		  (swap! elapsed inc))
     (Thread/sleep 1)))
 
 (defn timer-panel []
   (let [elapsed       (atom 0)
-        max           (atom 200)
-        progress-bar  (progress-bar :value 0 :max @max)
+        duration      (atom 200)
+        progress-bar  (progress-bar :value 0 :max @duration)
         elapsed-label (label :text "0ms")
-        slider        (slider :value @max :min 1 :max 400)
+        slider        (slider :value @duration :min 1 :max 400)
         reset         (button :text "Reset")]
     (b/bind elapsed progress-bar)
     (b/bind 
       elapsed
       (b/transform #(format-elapsed %))
       elapsed-label)
-    (b/bind slider max)
+    (b/bind slider duration)
     (b/bind
-      (b/funnel elapsed max)
-      (b/transform (fn [_] (Math/max @elapsed @max)))
+      (b/funnel elapsed duration)
+      (b/transform (fn [[e d]] (Math/max (or e @elapsed) (or d @duration))))
       (b/property progress-bar :max))
     (listen reset :action (fn [_] (reset! elapsed 0)))
-    (-> #(processing elapsed max) Thread. .start)
+    (-> #(processing elapsed duration) Thread. .start)
     (grid-panel 
       :rows 4
       :border (empty-border :thickness 10)
---
+++ Java7-Swing/src/javaguis/timer/Timer.java
@@ -0,0 +1,97 @@
+package javaguis.timer;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class Timer extends JFrame {
+    int elapsed, duration;
+    JProgressBar progressBar;
+    JLabel elapsedLabel;
+    JSlider durationSlider;
+    JButton resetButton;
+    
+    public Timer(String name) {
+        super(name);
+        elapsed = 0;
+        duration = 200;
+        initGUI();
+        initListeners();
+    }
+    
+    private void initGUI() {
+        progressBar = new JProgressBar(0, duration);
+        elapsedLabel = new JLabel("0s");
+        durationSlider = new JSlider(1, 400, duration);
+        resetButton = new JButton("Reset");
+
+        JPanel pane = new JPanel(new GridLayout(4, 0));
+        pane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.LINE_AXIS));
+        p.add(new JLabel("Elapsed Time: "));
+        p.add(progressBar);
+        pane.add(p);
+        pane.add(elapsedLabel);
+        p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.LINE_AXIS));
+        p.add(new JLabel("Duration: "));
+        p.add(durationSlider);
+        pane.add(p);
+        pane.add(resetButton);
+        this.add(pane);
+    }
+    
+    private void initListeners() {
+        javax.swing.Timer timer = new javax.swing.Timer(100, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (elapsed < duration) {
+				    elapsed++;
+                }
+				progressBar.setValue(elapsed);
+				progressBar.setMaximum(duration);
+				elapsedLabel.setText(formatElapsed(elapsed));
+            }
+        });
+        timer.start();
+        durationSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+			    duration = (int) Math.max(elapsed, durationSlider.getValue());
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                elapsed = 0;
+                duration = (int) durationSlider.getValue();
+            }
+        });
+    }
+    
+    private static String formatElapsed(int elapsed) {
+        int seconds = (int) Math.floor(elapsed / 10.0);
+        int dezipart = elapsed % 10;
+        return seconds + "." + dezipart + "s";
+    }
+    
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Timer frame = new Timer("Timer");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
