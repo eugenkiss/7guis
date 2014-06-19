efda158ff18d4e2b6f88f7a5470fb737a9dee38b
Eugen Kiss <eugen@eugenkiss.com>
2014-06-19 11:09:53 +0200

    Add new ReactFX Java version for Flight Booker

--- Java8-JavaFX/src/sevenguis/flightbooker/FlightBookerReactFX.java
+++ Java8-JavaFX/src/sevenguis/flightbooker/FlightBookerReactFX.java
@@ -1,7 +1,6 @@
 package sevenguis.flightbooker;
 
 import javafx.application.Application;
-import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,47 +9,47 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.reactfx.EventStream;
-import org.reactfx.EventStreams;
 
 import java.time.LocalDate;
 
 import static sevenguis.flightbooker.Util.*;
+import static org.reactfx.EventStreams.*;
 
 public class FlightBookerReactFX extends Application {
 
     public void start(Stage stage) {
-        ComboBox<String> flightType = new ComboBox<>();
+        ComboBox<String> flightType = new ComboBox<String>();
         flightType.getItems().addAll("one-way flight", "return flight");
-        TextField startDate = new TextField(dateToString(LocalDate.now()));
-        TextField returnDate = new TextField(dateToString(LocalDate.now()));
+        // If I don't set flightType's value to return flight here,
+        // the later setting to one-way flight is not picked up apparently...
+        flightType.setValue("return flight");
+        TextField startDate = new TextField();
+        TextField returnDate = new TextField();
         Button book = new Button("Book");
 
-        // We see that a cousin of FRP like ReactFX is not suited for the expression
-        // of functional dependencies. Their strengths lie elsewhere.
-        EventStream<String> flightTypeStream = EventStreams.valuesOf(flightType.valueProperty());
-        EventStream<String> startDateStream = EventStreams.valuesOf(startDate.textProperty());
-        EventStream<String> returnDateStream = EventStreams.valuesOf(returnDate.textProperty());
-        returnDate.disableProperty().bind(
-                flightTypeStream.map("one-way flight"::equals).toBinding(true));
-        // Using binding support of ReactFX
-        startDate.styleProperty().bind(startDateStream.map( s ->
-                isDateString(startDate.getText()) ? "" : "-fx-background-color: lightcoral"
-                ).toBinding(""));
-        // Alternatively,  with inversion of control a la callback
-        returnDateStream.map(s ->
-                isDateString(startDate.getText()) ? "" : "-fx-background-color: lightcoral"
-                ).subscribe(returnDate::setStyle);
-        // Combinators come into play (iov) (somewhat similar to Seesaw's funneling)
-        EventStreams.combine(flightTypeStream, startDateStream, returnDateStream).map((f, s, r) -> {
-            if ("one-way flight".equals(f)) {
-                return !isDateString(s);
-            } else {
-                return !isDateString(s) ||
-                       !isDateString(r) ||
-                       stringToDate(s).compareTo(stringToDate(r)) > 0;
-            }
-        }).subscribe(book::setDisable);
+        EventStream<Boolean> oneWay = valuesOf(flightType.valueProperty()).map(v -> v.equals("one-way flight"));
+        EventStream<LocalDate> startDateDate = valuesOf(startDate.textProperty())
+                .map(txt -> isDateString(txt) ? stringToDate(txt) : null);
+        EventStream<LocalDate> returnDateDate = valuesOf(returnDate.textProperty())
+                .map(txt -> isDateString(txt) ? stringToDate(txt) : null);
+        EventStream<Boolean> startDateValid = startDateDate.map(v -> v != null);
+        EventStream<Boolean> returnDateValid = returnDateDate.map(v -> v != null);
+        EventStream<Boolean> dateRangeValid = combine(startDateDate, returnDateDate).map((s, r) ->
+                s != null && r != null && s.compareTo(r) <= 0
+        );
+        EventStream<Boolean> datesValid = combine(oneWay, startDateValid, dateRangeValid)
+                .map((o, s, r) -> o && s || r);
+
+        oneWay.subscribe(returnDate::setDisable);
+        startDateValid.map(v -> v ? "" : "-fx-background-color: lightcoral").subscribe(startDate::setStyle);
+        returnDateValid.map(v -> v ? "" : "-fx-background-color: lightcoral").subscribe(returnDate::setStyle);
+        datesValid.map(v -> !v).subscribe(book::setDisable);
+
+        // It is very important to initialize the values of the widgets *after* the definition of the
+        // functional dependencies due to combine's semantics!
         flightType.setValue("one-way flight");
+        startDate.setText(dateToString(LocalDate.now()));
+        returnDate.setText(dateToString(LocalDate.now()));
 
         VBox root = new VBox(10, flightType, startDate, returnDate, book);
         root.setPadding(new Insets(10));
