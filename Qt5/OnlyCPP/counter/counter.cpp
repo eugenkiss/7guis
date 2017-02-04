4cb4f502b07bad26d4e398f7ab9e1f407ded515d
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2017-02-04 22:58:58 +0100

    Fix memory leaks

--- Qt5/OnlyCPP/counter/counter.cpp
+++ Qt5/OnlyCPP/counter/counter.cpp
@@ -2,23 +2,23 @@
 
 int main(int argc, char** argv)
 {
-	QApplication app{argc, argv};
-	auto window = new QMainWindow;
-	auto widget = new QWidget;
-	window->setCentralWidget(widget);
-	auto layout = new QHBoxLayout{widget};
-	auto label = new QLabel{"0"};
-	auto pushbutton = new QPushButton{"Count"};
-	
-	uint32_t count{};
-	QObject::connect(pushbutton, &QPushButton::clicked,
-			[&label, &count] () {
-				label->setText(QString::number(++count));
-			});
-	
-	layout->addWidget(label);
-	layout->addWidget(pushbutton);
-	
-	window->show();
-	return app.exec();
+  QApplication app{argc, argv};
+  QMainWindow window;
+  auto widget = new QWidget;
+  window.setCentralWidget(widget);
+  auto layout = new QHBoxLayout{widget};
+  auto label = new QLabel{"0"};
+  auto pushbutton = new QPushButton{"Count"};
+
+  uint32_t count{};
+  QObject::connect(pushbutton, &QPushButton::clicked,
+      [&label, &count] () {
+        label->setText(QString::number(++count));
+      });
+
+  layout->addWidget(label);
+  layout->addWidget(pushbutton);
+
+  window.show();
+  return app.exec();
 }
