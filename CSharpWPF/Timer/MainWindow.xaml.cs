fe16666e5e133c1e6a8a5bad753336d944ecd447
dzmitry.lahoda <dzmitry.lahoda@gmail.com>
2017-12-26 19:11:12 +0300

    finished timer koan

--- CSharpWPF/Timer/MainWindow.xaml.cs
+++ CSharpWPF/Timer/MainWindow.xaml.cs
@@ -27,35 +27,38 @@ namespace Timer
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+            => Calculate(0.0);
 
         private void S_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            
-        }
+            => Calculate(double.Parse(elapsed.Text));
 
         private void Grid_Initialized(object sender, EventArgs e)
-            => ((DispatcherTimer)this.Resources["timer"]).Start();         
+            => ((DispatcherTimer)this.Resources["timer"]).Start();
 
         private void DispatcherTimer_Tick(object sender, EventArgs e)
+            => Calculate((DispatcherTimer)sender);
+
+        private void Calculate(DispatcherTimer timer)
         {
-            var timer = (DispatcherTimer)sender;
-            if (G.Value >= S.Value)
+            var elapsedValue = double.Parse(elapsed.Text);
+            var newValue = elapsedValue + timer.Interval.TotalSeconds;
+            Calculate(newValue);
+        }
+
+        private void Calculate(double newValue)
+        {
+            if (Double.IsNaN(S.Value) || S.Value <= 0)
             {
                 var update = 0.0;
-                G.Value = 0.0;
+                G.Value = update;
                 elapsed.Text = update.ToString("F1");
             }
             else
             {
-                var newValue = G.Value + timer.Interval.TotalSeconds;
-                var update = newValue > G.Maximum ? G.Maximum : newValue;
-                G.Value = update;
+                var update = newValue > S.Value ? 0 : newValue;
+                G.Value = (update / S.Value) * G.Maximum;
                 elapsed.Text = update.ToString("F1");
             }
-
         }
     }
 }
