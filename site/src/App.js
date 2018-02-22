5fe126cf5d7d3f4d37b667c8e59bf7de8140eaa0
Eugen Kiss <eugen@eugenkiss.com>
2018-02-22 18:03:49 +0100

    Small fixes

--- site/src/App.js
+++ site/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, } from 'react-static'
+import { Router } from 'react-static'
 import Routes from 'react-static-routes'
 import {css, injectGlobal} from 'emotion'
 
--- site/src/containers/404.js
+++ site/src/containers/404.js
@@ -1,6 +1,6 @@
 import React from 'react'
-//
 
+// noinspection JSUnusedGlobalSymbols
 export default () => (
   <div>
     <h1>404 - Not found</h1>
