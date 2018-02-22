e47a749ebee539da7c2ea9213f84dfc4dc72a0fb
Eugen Kiss <eugen@eugenkiss.com>
2018-02-22 15:47:56 +0100

    Add favicon

--- site/static.config.js
+++ site/static.config.js
@@ -90,6 +90,8 @@ export default {
             <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <title>7GUIs</title>
+            <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+            <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
             <style>{renderMeta.css}</style>
           </Head>
           <Body>
