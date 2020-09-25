6f388814ce12162b0fd7c312f9ccab11524a6fee
petelomax <petelomax@ymail.com>
2020-09-25 14:48:50 +0100

    Update Implementations.js

--- site/src/containers/Implementations.js
+++ site/src/containers/Implementations.js
@@ -184,10 +184,10 @@ const implementations = [
   {
     title: 'Phix',
     technologies: ['Phix'],
-    author: 'petelomax NA',
-    authorLink: 'https://bitbucket.org/petelomax/',
-    link: 'https://bitbucket.org/petelomax/phix/src/default/demo/rosetta/7guis/',
-    src: 'https://bitbucket.org/petelomax/phix/src/default/demo/rosetta/7guis/',
+    author: 'Pete Lomax',
+    authorLink: 'https://github.com/petelomax',
+    link: 'https://github.com/petelomax/Phix/tree/master/demo/rosetta/7guis',
+    src: 'https://github.com/petelomax/Phix/tree/master/demo/rosetta/7guis',
   },
   {
     title: 'VFP',
