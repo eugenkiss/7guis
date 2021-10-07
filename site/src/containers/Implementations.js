7b430d58bd7d84ec955aa2339390db51bb09d08f
Cris <cris7fe@gmail.com>
2021-10-07 13:47:58 +0800

    Add BANG! web components implementation

--- site/src/containers/Implementations.js
+++ site/src/containers/Implementations.js
@@ -306,5 +306,13 @@ const implementations = [
     authorLink: "https://github.com/bradwoods/",
     link: "https://7guis.bradwoods.io",
     src: "https://github.com/bradwoods/7guis-html-css-js",
+  },
+  {
+    title: "BANG!",
+    technologies: ["BANG!", "Web Components"],
+    author: "Cris",
+    authorLink: "https://github.com/i5ik/",
+    link: "https://i5ik.github.io/_____/7guis/",
+    src: "https://github.com/i5ik/_____/tree/main/docs/7guis",
   }
 ]
